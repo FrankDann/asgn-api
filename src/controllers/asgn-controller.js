9e8f2e8fa43fb28fd7228b05b5c19bb24989e312
Frank Dann <frank.a.dann@gmail.com>
2019-05-07 15:41:13 -0500

    Change asgn-controller

---
+++ src/controllers/asgn-controller.js
@@ -0,0 +1,68 @@
+var Asgn = require("../src/models/asgn-model");
+
+exports.AddAsgn = function(req, res) {
+    if(!req.body)
+    {
+        return res.status(400).send("Req body missing");
+    }
+    var model = new Asgn(req,body);
+    model.save()
+        .then((doc) => {
+            if(!doc || doc.length == 0)
+            {
+                return res.status(500).send("Server error.");
+            }
+            res.status(201).send(doc);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+}
+
+exports.ListAsgn = function(req,res) {
+    if(!req.body)
+    {
+        return res.status(400).send("Req body missing");
+    }
+    Asgn.findOne({
+        assignmentName: req.query.assignmentName
+    })
+        .then((doc) => {
+            res.json(doc);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+}
+
+exports.UpdateAsgn = function(req,res) {
+    if(!req.body)
+    {
+        return res.status(400).send("Req body missing");
+    }
+    Asgn.findOneAndUpdate({
+        assignmentName: req.query.assignmentName
+    }, req.body, {new: true} )
+        .then((doc) => {
+            res.json(doc);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+}
+
+exports.DeleteAsgn = function(req,res) {
+    if(!req.body)
+    {
+        return res.status(400).send("Req body missing");
+    }
+    Asgn.findOneAndDelete({
+        assignmentName: req.query.assignmentName
+    })
+        .then((doc) => {
+            res.json(doc);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+}
