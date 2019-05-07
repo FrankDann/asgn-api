46fbf108d79d069df912daf2b73f1b92039d1081
Frank Dann <frank.a.dann@gmail.com>
2019-05-07 15:20:17 -0500

    Change asgn-model

--- src/models/asgn-model.js
+++ src/models/asgn-model.js
@@ -1,1 +1,13 @@
 var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var AsgnSchema = new Schema({
+    courseName: String,
+    assignmentName: {
+        type: String,
+        required: true
+    },
+    dueDate: Date
+});
+
+module.exports = mongoose.model('Asgn', AsgnSchema);
